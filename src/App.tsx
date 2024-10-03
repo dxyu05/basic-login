1cc7ceca2706ca2caa5720a5641b232abc0488e2
dxyu05 <dyu12@terpmail.umd.edu>
2024-10-03 19:14:27 -0400

    flkjas;dfklja;ldfkj

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,7 @@ const App: React.FC = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    if (name && password) {
-      setIsLoggedIn(true);
-    }
+
   };
 
   return (
@@ -26,22 +23,169 @@ const App: React.FC = () => {
               type="text"
               id="name"
               value={name}
-              onChange={(e) => setName(e.target.value)}
               required
             />
           </div>
           <div>
+            <img src="https://us.schleich-s.com/cdn/shop/products/20842_main_v23_TP.jpg?v=1695745770" alt="Italian Trulli"></img>
             <label htmlFor="password">Password:</label>
+          </div>
             <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+                type="text"
+                id="name"
+                value={name}
+                required
+              />
+          <button type="submit">Enter</button>
+          <h1>Login</h1>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
               required
             />
           </div>
+          <div>
+            <img src="https://us.schleich-s.com/cdn/shop/products/20842_main_v23_TP.jpg?v=1695745770" alt="Italian Trulli"></img>
+            <label htmlFor="password">Password:</label>
+          </div>
+            <input
+                type="text"
+                id="name"
+                value={name}
+                required
+              />
+          <button type="submit">Enter</button>
+          <h1>Login</h1>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              required
+            />
+          </div>
+          <div>
+            <img src="https://us.schleich-s.com/cdn/shop/products/20842_main_v23_TP.jpg?v=1695745770" alt="Italian Trulli"></img>
+            <label htmlFor="password">Password:</label>
+          </div>
+            <input
+                type="text"
+                id="name"
+                value={name}
+                required
+              />
+          <button type="submit">Enter</button>
+          <h1>Login</h1>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              required
+            />
+          </div>
+          <div>
+            <img src="https://us.schleich-s.com/cdn/shop/products/20842_main_v23_TP.jpg?v=1695745770" alt="Italian Trulli"></img>
+            <label htmlFor="password">Password:</label>
+          </div>
+            <input
+                type="text"
+                id="name"
+                value={name}
+                required
+              />
+          <button type="submit">Enter</button>
+          <h1>Login</h1>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              required
+            />
+          </div>
+          <div>
+            <img src="https://us.schleich-s.com/cdn/shop/products/20842_main_v23_TP.jpg?v=1695745770" alt="Italian Trulli"></img>
+            <label htmlFor="password">Password:</label>
+          </div>
+            <input
+                type="text"
+                id="name"
+                value={name}
+                required
+              />
+          <button type="submit">Enter</button>
+          <h1>Login</h1>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              required
+            />
+          </div>
+          <div>
+            <img src="https://us.schleich-s.com/cdn/shop/products/20842_main_v23_TP.jpg?v=1695745770" alt="Italian Trulli"></img>
+            <label htmlFor="password">Password:</label>
+          </div>
+            <input
+                type="text"
+                id="name"
+                value={name}
+                required
+              />
+          <button type="submit">Enter</button>
+          <h1>Login</h1>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              required
+            />
+          </div>
+          <div>
+            <img src="https://us.schleich-s.com/cdn/shop/products/20842_main_v23_TP.jpg?v=1695745770" alt="Italian Trulli"></img>
+            <label htmlFor="password">Password:</label>
+          </div>
+            <input
+                type="text"
+                id="name"
+                value={name}
+                required
+              />
+          <button type="submit">Enter</button>
+          <h1>Login</h1>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              required
+            />
+          </div>
+          <div>
+            <img src="https://us.schleich-s.com/cdn/shop/products/20842_main_v23_TP.jpg?v=1695745770" alt="Italian Trulli"></img>
+            <label htmlFor="password">Password:</label>
+          </div>
+            <input
+                type="text"
+                id="name"
+                value={name}
+                required
+              />
           <button type="submit">Enter</button>
         </form>
+        
       )}
     </div>
   );
